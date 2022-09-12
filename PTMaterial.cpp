c95c899b51e5f91d7d2d3337e8161e3eeeadbbd9
SleepinWei <zyw0241@outlook.com>
2022-09-12 13:08:58 +0800

    A: add Metallic and Dielectric Material

--- PTMaterial.cpp
+++ PTMaterial.cpp
@@ -1,37 +1,69 @@
 #include"PTMaterial.h"
 
-PT::vec3 PT::random_in_hemisphere(PT::vec3& normal) {
-		PT::vec3 random_dir = PT::random_direction();
-		if (dot(random_dir, normal) > 0.0) {
-			return random_dir;
-		}
-
-		return -random_dir;
-	}
-
 PT::Lambertian::Lambertian(const vec3& a):albedo(a){
 }
 bool PT::Lambertian::scatter(
-	Ray& r,hitRecord & rec,
+	const Ray& r,const hitRecord & rec,
 	vec3& attenuation, 
 	Ray& scattered
-) 
+) const 
 {
-	auto scatter_direction = random_in_hemisphere(rec.normal);
+	//auto scatter_direction = random_in_hemisphere(rec.normal);
+	auto scatter_direction = rec.normal + random_in_unit_sphere();
+	if (nearZero(scatter_direction)) {
+		scatter_direction = rec.normal;
+	}
+	else {
+		scatter_direction = normalize(scatter_direction);
+	}
 	scattered = Ray(rec.p, scatter_direction);
 	attenuation = albedo;
 	return true;
 }
-PT::Metal::Metal(const vec3 & a):albedo(a){
+PT::Metal::Metal(const vec3 & a,double f):albedo(a),fuzz(f<1?f:1) {
 }
 bool PT::Metal::scatter(
-	Ray& r, hitRecord& rec,
+	const Ray& r, const hitRecord& rec,
 	vec3& attenuation,
 	Ray& scattered
-	) {
+	) const {
 	vec3 dir = normalize(r.dir);
 	vec3 reflected = reflect(dir, rec.normal);
-	scattered = Ray(rec.p, reflected);
+	scattered = Ray(rec.p, reflected + fuzz * random_in_unit_sphere());
 	attenuation = albedo;
 	return (dot(scattered.dir, rec.normal) > 0);
 }
+PT::Dielectric::Dielectric(double index_of_refraction) : ir(index_of_refraction) {
+}
+
+bool PT::Dielectric::scatter(
+	const Ray& r_in, const hitRecord& rec, vec3& attenuation, Ray& scattered
+)const {
+	attenuation = vec3(1.0, 1.0, 1.0);
+	double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+	vec3 unit_direction = normalize(r_in.dir);
+	//vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+	double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+	double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+	bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+	vec3 direction;
+
+	if (cannot_refract||reflectance(cos_theta,refraction_ratio)>random_double())
+	//	// 按照概率反射，概率为 reflectance
+		direction = reflect(unit_direction, rec.normal);
+	else
+		direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+	scattered = Ray(rec.p, normalize(direction));
+	return true;
+}
+double PT::Dielectric::reflectance(double cosine, double ref_idx) {
+	// Use Schlik's approximation
+	auto r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+	
+
--- PTMaterial.h
+++ PTMaterial.h
@@ -3,22 +3,21 @@
 #include"hittable.h"
 
 namespace PT {
-	vec3 random_in_hemisphere(PT::vec3& normal);
 	
 	class Material {
 	public:
 		virtual bool scatter(
-			Ray& r_in, hitRecord& rec, vec3& attenuation, Ray& scattered) = 0;
+			const Ray& r_in, const hitRecord& rec, vec3& attenuation, Ray& scattered) const= 0;
 	};
 
 	class Lambertian :public Material {
 	public:
 		Lambertian(const vec3& a);
 		virtual bool scatter(
-			Ray& r, hitRecord& rec,
+			const Ray& r, const hitRecord& rec,
 			vec3& attenuation,
 			Ray& scattered
-		);
+		)const override;
 		
 	public:
 		vec3 albedo;
@@ -26,13 +25,26 @@ namespace PT {
 
 	class Metal : public Material {
 	public:
-		Metal(const vec3& a);
+		Metal(const vec3& a,double f);
 		virtual bool scatter(
-			Ray& r, hitRecord& rec,
+			const Ray& r, const hitRecord& rec,
 			vec3& attenuation,
 			Ray& scattered
-		) override;
+		) const override;
 	public:
 		vec3 albedo;
+		double fuzz;
+	};
+	class Dielectric : public Material {
+	public:
+		Dielectric(double index_of_refraction);
+
+		virtual bool scatter(
+			const Ray& r, const hitRecord& rec, vec3& attenuation, Ray& scattered
+		)const override;
+	public:
+		double ir; 
+	private:
+		static double reflectance(double cosine, double ref_idx);
 	};
 }
