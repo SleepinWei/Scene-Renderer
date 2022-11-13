863c1ea6cb630f25e03fc918f143bdf865c8b26d
SleepinWei <zyw0241@outlook.com>
2022-11-13 16:57:27 +0800

    A: add box

---
+++ src/PT/hittable/Box.cpp
@@ -0,0 +1,44 @@
+#include"Box.h"
+#include"../PTRay.h"
+#include"../PTAABB.h"
+#include"../PTRay.h"
+#include"Rect.h"
+
+using namespace PT;
+
+Box::Box(){
+	model = glm::mat4(1.0f);
+}
+
+bool Box::bounding_box(double time0, double time1, AABB& output_box) const {
+	//vec3 
+	output_box = AABB(box_min, box_max);
+	return true;
+}
+
+void Box::setModel(glm::mat4 model) {
+	this->model = model;
+}
+
+Box::Box(const point3& p0, const point3& p1, shared_ptr<Material> ptr) {
+	box_min = p0;
+	box_max = p1;
+
+	sides.add(make_shared<xy_rect>(p0.x, p1.x, p0.y, p1.y, p1.z, ptr));
+	sides.add(make_shared<xy_rect>(p0.x, p1.x, p0.y, p1.y, p0.z, ptr));
+
+	sides.add(make_shared<xz_rect>(p0.x, p1.x, p0.z, p1.z, p1.y, ptr));
+	sides.add(make_shared<xz_rect>(p0.x, p1.x, p0.z, p1.z, p0.y, ptr));
+
+	sides.add(make_shared<yz_rect>(p0.y, p1.y, p0.z, p1.z, p1.x, ptr));
+	sides.add(make_shared<yz_rect>(p0.y, p1.y, p0.z, p1.z, p0.x, ptr));
+}
+
+bool Box::hit(const Ray& r, double t_min, double t_max, hitRecord& rec) const {
+	return sides.hit(r, t_min, t_max, rec);
+}
+
+
+
+
+
---
+++ src/PT/hittable/Box.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<glm/glm.hpp>
+#include<memory>
+
+#include"../hittable.h"
+
+using glm::vec3;
+using point3 = glm::vec3;
+using std::shared_ptr;
+
+namespace PT {
+    class Material;
+    class Ray;
+    class hitRecord;
+    class AABB;
+
+    class Box: public hittable {
+    public:
+        Box();
+		Box(const point3& p0, const point3& p1, shared_ptr<Material> ptr);
+
+        void setModel(glm::mat4 model);
+
+        virtual bool hit(const Ray& r, double t_min, double t_max, hitRecord& rec) const override;
+
+        virtual bool bounding_box(double time0, double time1, AABB& output_box) const override;
+    public:
+        // TODO: add model transformation to all;
+        glm::mat4 model;
+
+        vec3 box_min;
+        vec3 box_max;
+        hittable_list sides;
+    };
+}
