17076321505edcad49263abae6f9fa727f595b54
XiaoX <875440114@qq.com>
2022-12-09 19:43:42 +0800

    setMat

--- src/renderer/RenderPass.cpp
+++ src/renderer/RenderPass.cpp
@@ -501,6 +501,15 @@ void RSMPass::render(const std::shared_ptr<RenderScene>& scene) {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	//TODO:set light uniform
+	glm::vec3 lightPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 lightColor = glm::vec3(0.6f, 0.6f, 0.6f);
+	glm::mat4 lightProjection = glm::perspective(glm::radians(60.0f), (float)RSM_WIDTH / (float)RSM_HEIGHT, light_near_plane, light_far_plane);
+	glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+	RSMShader->use();
+	RSMShader->setMat4("lightSpaceMatrix", lightSpaceMatrix);
+	RSMShader->setVec3("light.Position", lightPos);
+	RSMShader->setVec3("light.Color", lightColor);
 
 	for (int i = 0; i < scene->objects.size(); i++) {
 		auto& object = scene->objects[i];
